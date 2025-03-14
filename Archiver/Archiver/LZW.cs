fb40e2f7e0821e57f0703f9fc93ce58c5053ef1e
PolinaLukianova <polinalukianovapl@gmail.com>
2025-03-14 23:04:47 +0300

    Homework 3

---
+++ Archiver/Archiver/LZW.cs
@@ -0,0 +1,128 @@
+﻿namespace Archiver;
+
+/// <summary>
+/// Implementation of the LZW data compression algorithm.
+/// </summary>
+public class LZW
+{
+    /// <summary>
+    /// File compression.
+    /// </summary>
+    /// <param name="inputPath">Path to input file to compress.</param>
+    /// <param name="outputPath">Path to output file.</param>
+    public static void Compress(string inputPath, string outputPath)
+    {
+        Trie trie = new();
+        trie.BaseInitialization();
+
+        using var inputStream = File.OpenRead(inputPath);
+        using var outputStream = File.Create(outputPath);
+        using BinaryWriter writer = new(outputStream);
+
+        Node current = trie.Root;
+        int bitWidth = 9;
+        List<int> indSet = [];
+        int byteR;
+
+        while ((byteR = inputStream.ReadByte()) != -1)
+        {
+            byte currentByte = (byte)byteR;
+
+            if (current.Child.TryGetValue(currentByte, out Node? next))
+            {
+                current = next;
+            }
+            else
+            {
+                indSet.Add(current.Index);
+                Node newNode = new(currentByte, trie.NextIndex);
+                trie.NextIndex++;
+                current.Child[currentByte] = newNode;
+
+                if (trie.NextIndex >= (1 << bitWidth) && bitWidth < 16)
+                {
+                    bitWidth++;
+                }
+
+                if (trie.Root.Child.TryGetValue(currentByte, out Node? node))
+                {
+                    current = node;
+                }
+                else
+                {
+                    current = trie.Root;
+                }
+            }
+        }
+
+        if (current != trie.Root)
+        {
+            indSet.Add(current.Index);
+        }
+
+        writer.Write(bitWidth);
+
+        foreach (var item in indSet)
+        {
+            byte[] buffer = BitConverter.GetBytes(item);
+            int countBytes = (bitWidth + 7) / 8;
+            writer.Write(buffer, 0, countBytes);
+        }
+
+        var inputLength = new FileInfo(inputPath).Length;
+        var outputLength = outputStream.Length;
+
+        Console.WriteLine($"Compression ratio: {(double)inputLength / outputLength:F3}");
+    }
+
+    /// <summary>
+    /// Uncompressing a file.
+    /// </summary>
+    /// <param name="inputPath">Path to input file to uncompress.</param>
+    /// <param name="outputPath">Path to output file.</param>
+    public static void Uncompress(string inputPath, string outputPath)
+    {
+        using BinaryReader reader = new(File.OpenRead(inputPath));
+
+        int bitWidth = reader.ReadInt32();
+        List<int> set = [];
+
+        while (reader.BaseStream.Position < reader.BaseStream.Length)
+        {
+            byte[] buffer = reader.ReadBytes((bitWidth + 7) / 8);
+            set.Add(BitConverter.ToInt32([.. buffer, .. new byte[4 - buffer.Length]], 0));
+        }
+
+        Dictionary<int, string> dictionary = [];
+        for (int i = 0; i < 256; ++i)
+        {
+            dictionary[i] = ((char)i).ToString();
+        }
+
+        string previous = dictionary[set[0]];
+        List<byte> output = [.. previous.Select(item => (byte)item)];
+        int nextIndex = 256;
+
+        for (int i = 1; i < set.Count; ++i)
+        {
+            string entry;
+            int index = set[i];
+
+            if (dictionary.TryGetValue(index, out entry!))
+            {
+                output.AddRange(entry.Select(item => (byte)item));
+            }
+            else if (index == nextIndex)
+            {
+                entry = previous + previous[0];
+                output.AddRange(entry.Select(item => (byte)item));
+            }
+
+            dictionary[nextIndex] = previous + entry[0];
+            nextIndex++;
+            previous = entry;
+        }
+
+        File.WriteAllBytes(outputPath, [.. output]);
+    }
+}
---
+++ Archiver/Archiver/Trie.cs
@@ -0,0 +1,41 @@
+﻿namespace Archiver;
+
+/// <summary>
+/// A trie data structure.
+/// </summary>
+public class Trie
+{
+    public int NextIndex;
+    private Node root = new(0);
+    private Node? current;
+
+    /// <summary>
+    /// Gets the root node of the Trie.
+    /// </summary>
+    public Node Root => this.root;
+
+    /// <summary>
+    /// Initializes the Trie with the first 256 ASCII characters.
+    /// </summary>
+    public void BaseInitialization()
+    {
+        for (int i = 0; i < 256; ++i)
+        {
+            this.Root.Child[(byte)i] = new Node((byte)i, i);
+        }
+
+        this.NextIndex = 256;
+        this.current = this.Root;
+    }
+
+    /// <summary>
+    /// Adds a new byte.
+    /// </summary>
+    /// <param name="nextByte">The byte to be added.</param>
+    public void Add(byte nextByte)
+    {
+        this.current.Child[nextByte] = new(nextByte, this.NextIndex);
+        this.NextIndex++;
+        this.current = this.Root;
+    }
+}
