891a7de1934eb158d307e1397ff86e8af030f121
PolinaLukianova <polinalukianovapl@gmail.com>
2025-03-17 23:27:48 +0300

    An entry point has been added.

--- Archiver/Archiver/Program.cs
+++ Archiver/Archiver/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿namespace Archiver;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inputPath = args[0];
+        string key = args[1];
+
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine("The file path is incorrect.");
+            return;
+        }
+
+        string outputPath;
+        if (key == "-c")
+        {
+            int lastDot = inputPath.LastIndexOf('.');
+            if (lastDot != -1)
+            {
+                outputPath = inputPath[..lastDot] + ".zipped";
+            }
+            else
+            {
+                outputPath = inputPath + ".zipped";
+            }
+
+            LZW.Compress(inputPath, outputPath);
+        }
+        else if (key == "-u")
+        {
+            if (!inputPath.EndsWith(".zipped"))
+            {
+                Console.WriteLine("Incorrect file path. Expected .zipped.");
+                return;
+            }
+
+            int lastDot = inputPath.LastIndexOf('.');
+            outputPath = inputPath[..lastDot];
+
+            LZW.Uncompress(inputPath, outputPath);
+        }
+    }
+}
