1a5bd700273180b305a78d68a7c105290db904b5
PolinaLukianova <polinalukianovapl@gmail.com>
2025-03-28 23:47:58 +0300

    Homework 5

---
+++ Routers/Routers/Edge.cs
@@ -0,0 +1,17 @@
+﻿namespace Routers;
+
+public class Edge(int begin, int end, int weight): IComparable<Edge>
+{
+    public int Weight { get; } = weight;
+    public int Begin { get; } = begin;
+    public int End { get; } = end;
+
+    public int CompareTo(Edge? other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        return other.Weight.CompareTo(Weight);
+    }
+}
