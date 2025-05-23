10e1d36f58c4ffca0dc3b8eae98f66ae43c47983
PolinaLukianova <polinalukianovapl@gmail.com>
2025-05-23 15:31:56 +0300

    Test-2

---
+++ GenericList/GenericList/MyList.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="MyList.cs" company="Polina Lukianova">
+// Copyright (c) Polina Lukianova. All rights reserved.
+// </copyright>
+
+namespace GenericList;
+
+using System.Collections;
+
+/// <summary>
+/// Generic list.
+/// </summary>
+/// <typeparam name="T">The type of elements in the list.</typeparam>
+public class MyList<T> : IEnumerable<T>, IEnumerable
+{
+    private T[] items;
+    private int count;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyList{T}"/> class.
+    /// </summary>
+    public MyList()
+    {
+        this.items = new T[4];
+        this.count = 0;
+    }
+
+    /// <summary>
+    /// Adds elements to the end of the list.
+    /// </summary>
+    /// <param name="item">Element to be added.</param>
+    public void Add(T item)
+    {
+        if (this.count == this.items.Length)
+        {
+            this.Resize();
+        }
+
+        this.items[this.count] = item;
+        this.count++;
+    }
+
+    /// <summary>
+    /// Returns a generic enumerator that iterates through the list.
+    /// </summary>
+    /// <returns>An enumerator for the list.</returns>
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.count; ++i)
+        {
+            yield return this.items[i];
+        }
+    }
+
+    /// <summary>
+    /// Returns a non-generic enumerator.
+    /// </summary>
+    /// <returns>An non-enumerator for the list.</returns>
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    private void Resize()
+    {
+        T[] newItems = new T[this.items.Length * 2];
+        Array.Copy(this.items, newItems, this.count);
+        this.items = newItems;
+    }
+}
---
+++ GenericList/GenericList/NullCounter.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="NullCounter.cs" company="Polina Lukianova">
+// Copyright (c) Polina Lukianova. All rights reserved.
+// </copyright>
+
+namespace GenericList;
+
+/// <summary>
+/// A static method to count elements in a list that satisfy a custom "null".
+/// </summary>
+public static class NullCounter
+{
+    /// <summary>
+    /// Count null elements in the list.
+    /// </summary>
+    /// <typeparam name="T">Type of elements in the list.</typeparam>
+    /// <param name="list">The list to search.</param>
+    /// <param name="isNull">A function that determines whether an element is considered "null".</param>
+    /// <returns>The number of null elements.</returns>
+    public static int CountNulls<T>(
+        MyList<T> list, Func<T, bool> isNull)
+    {
+        int count = 0;
+        foreach (var item in list)
+        {
+            if (isNull(item))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
